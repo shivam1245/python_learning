ef11ac024a128750fcfff6776ddc8d5cf2919831
shivam maurya <mauryashivam1245@gmail.com>
2023-02-01 08:53:36 +0530

    Complex function removed
    complex addition does not work properly

--- Calculator/calc_fun.py
+++ Calculator/calc_fun.py
@@ -40,13 +40,6 @@ def pow() :
     s=num1 ** num2
     print(f"{ num1 }to the power { num1}= ",s)
 
-def complax():
-    num1=float(input("Enter the first number.......  "))
-    num2=float(input("Enter the second number.......  "))
-    a=num1 + num3
-    b=num2 + num4
-    print(num1,op,num2,"= ",s)
-
 def exp() :
     num1=float(input("Enter the power of e.......  "))
     
